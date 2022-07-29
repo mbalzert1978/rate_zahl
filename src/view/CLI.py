bd8e034315aeccfc44763fd3d727d27d2a18e022
Markus Iorio <mbalzert@gmail.com>
2022-07-30 01:05:59 +0200

    first commit CLI

--- src/view/CLI.py
+++ src/view/CLI.py
@@ -1,2 +1,22 @@
-class CLI:
-    pass
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from src.app import App
+from src.view.view import View
+
+if TYPE_CHECKING:
+    from src.app import App
+
+
+class CLI(View):
+    @property
+    def app(self):
+        return self.__app
+
+    @app.setter
+    def app(self, value):
+        if not isinstance(value, App):
+            raise ValueError(f"No valid Viewinstace: {value}")
+        self.__app = value
+
+    def display_header(self) -> None:
+        raise NotImplementedError()
