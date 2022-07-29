a6df67fead626fb7195b1a574f0e33c9d937d938
Markus Iorio <mbalzert@gmail.com>
2022-07-30 00:43:55 +0200

    game in gamelogic umbenannt

---
+++ src/game/gamelogic.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+
+class GameLogic(ABC):
+    @abstractmethod
+    def gues(self):
+        """make a gues"""
+
+    @abstractmethod
+    def is_winner(self):
+        """checks if there is a winner"""
