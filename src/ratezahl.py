b144379033b91759b3e5eddd60f5ff8a19930aba
Markus Iorio <mbalzert@gmail.com>
2022-07-30 00:44:16 +0200

    game in gamelogic umbenannt

--- src/ratezahl.py
+++ src/ratezahl.py
@@ -2,15 +2,13 @@ from __future__ import annotations
 import random
 from src.game.gamelogic import GameLogic
 from src.view.view import View
-    from src.player.players import Player
-    from src.app import App
-    from src.game.game import Game
-    from src.view.view import View
+from src.player.players import Player
+from src.app import App
 
 
 class RateZahl(App):
     def __init__(self, game, view, players) -> None:
-        self.game: Game = game
+        self.game: GameLogic = game
         self.view: View = view
         self.players: list[Player] = players
         self.active_player: Player = random.choice(self.players)
@@ -23,7 +21,7 @@ class RateZahl(App):
     def players(self, value: list[Player]) -> None:
         if not isinstance(value, list):
             raise ValueError(f"No valid Playerlist: {value}")
-        if not isinstance(value, Game):
+        if not isinstance(next(iter(value)), Player):
             raise ValueError(f"No valid Playerlist: {value}")
         self.__players = value
 
@@ -42,8 +40,8 @@ class RateZahl(App):
         return self.__game
 
     @game.setter
-    def game(self, value: Game) -> None:
-        if not isinstance(value, Game):
+    def game(self, value: GameLogic) -> None:
+        if not isinstance(value, GameLogic):
             raise ValueError(f"No valid Gameinstace: {value}")
         self.__game = value
 
