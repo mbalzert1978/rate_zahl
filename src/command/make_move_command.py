2043f9a23b101f7270392e007702d835e23adbe9
Markus Iorio <mbalzert@gmail.com>
2022-07-29 23:18:40 +0200

    implementierung von make move command

---
+++ src/command/make_move_command.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from src.app import App
+    from src.game.game import Game
+from src.command.command import Command
+
+
+class MakeMove(Command):
+    @property
+    def app(self):
+        return self.__app
+
+    @app.setter
+    def app(self, value: App):
+        if not isinstance(value, App):
+            raise ValueError(f"No valid App: {value}")
+        self.__app = value
+
+    @property
+    def logic(self):
+        return self.__logic
+
+    @logic.setter
+    def logic(self, value: Game):
+        if isinstance(value, Game):
+            self.__logic = value
+        raise ValueError(f"No valid Gamelogic: {value}")
