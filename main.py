310973bf7a94307095a8e908b3c5c4592ecaf9cf
Markus Iorio <mbalzert@gmail.com>
2022-07-30 00:39:01 +0200

    implementierung von game setup

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from src.game.ratezahllogic import RateZahlLogic
 from src.ratezahl import RateZahl
 from src.view.view import View
 from src.player.players import HumanPlayer, CPUPlayer
+
+
+def setup_game():
+    l = RateZahlLogic()
+    v = View()
+    p = [HumanPlayer(3), CPUPlayer(3)]
+    app = RateZahl(l, v, p)
+    return app
+
+
 def app():
     pass
 
