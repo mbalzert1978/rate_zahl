1d2bbcad204043088cb8768873e7610b7ca2c382
Markus Iorio <mbalzert@gmail.com>
2022-07-29 23:15:11 +0200

    neue main im aufbau

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-from player import HumanPlayer, RandomCPU
-from view import GuessTheNumberCLI
-from controller import GuessTheNumberController
-from model import GuessTheNumberGameModel
-
-
-def main():
-    game = setup_game()
-    game.play()
-
-
-def setup_game():
-    m = GuessTheNumberGameModel()
-    v = GuessTheNumberCLI()
-    list_of_players = [HumanPlayer(), RandomCPU()]
-    app = GuessTheNumberController(model=m, view=v, players=list_of_players)
-    return app
+def app():
+    pass
 
 
 if __name__ == "__main__":
-    main()
+    app()
