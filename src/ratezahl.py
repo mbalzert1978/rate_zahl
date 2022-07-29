b5ef8b325ed8e0ca6e4f39456e78a6461c3bacd5
Markus Iorio <mbalzert@gmail.com>
2022-07-29 23:17:57 +0200

    erste implementierung von RateZahlApp

---
+++ src/ratezahl.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from src.app import App
+    from src.game.game import Game
+from src.app import App
+
+
+class RateZahl(App):
+    @property
+    def game(self) -> None:
+        return self.__game
+
+    @game.setter
+    def game(self, value: Game) -> None:
+        if not isinstance(value, Game):
+            raise ValueError(f"No valid Gameinstace: {value}")
+        self.__game = value
+
+    def create_ui(self) -> None:
+        raise NotImplementedError
+
+    def play(self) -> None:
+        raise NotImplementedError
