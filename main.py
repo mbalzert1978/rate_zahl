d0b6bd504eb889a76fd4f066ac7366c4bad01ab1
Markus Iorio <mbalzert@gmail.com>
2022-07-30 00:38:25 +0200

    setup imports

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from src.game.ratezahllogic import RateZahlLogic
+from src.ratezahl import RateZahl
+from src.view.view import View
+from src.player.players import HumanPlayer, CPUPlayer
 def app():
     pass
 
